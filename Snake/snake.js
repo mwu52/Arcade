572f5525fdaecfb8cef2ed98333fe5b4532ff5cd
Michael Wu <mwu52@uic.edu>
2022-06-26 21:31:05 -0500

    update

---
+++ Snake/snake.js
@@ -0,0 +1,7 @@
+
+    document.addEventListener("DOMContentLoaded", function () {
+        pTag = document.querySelector("div");
+        newVal = document.createElement("p");
+        newVal.innerHTML = '';
+        pTag.appendChild(newVal);
+      });
